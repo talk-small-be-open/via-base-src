c9e2c1f224d0fb2bf9c085acebc5ab7fb6dca3d5
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2020-05-05 17:03:28 +0200

    imported web_root folder

---
+++ web_root/files/css/.dir-locals.el
@@ -0,0 +1,8 @@
+(
+ (less-css-mode . (
+									 (subdirs . nil)
+									 (less-css-input-file-name . "styles.less")
+									 (less-css-output-file-name . "styles.css")
+									 (less-css-output-directory . ".")
+									 ))
+ )
