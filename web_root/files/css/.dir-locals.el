d723402325d3318f9a78094ee4403088c60e7916
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2020-05-08 15:37:36 +0200

    reorganizing folders

--- web_root/files/css/.dir-locals.el
+++
@@ -1,8 +0,0 @@
-(
- (less-css-mode . (
-									 (subdirs . nil)
-									 (less-css-input-file-name . "styles.less")
-									 (less-css-output-file-name . "styles.css")
-									 (less-css-output-directory . ".")
-									 ))
- )
