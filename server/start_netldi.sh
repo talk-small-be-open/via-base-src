8b2cdf2fbb612854b883f7885a1089eaf5e4ac3b
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2020-05-18 13:35:54 +0200

    fix starting netldi from monit

--- server/start_netldi.sh
+++ server/start_netldi.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# starting netldi needs the USER env var. If running from monit, that var is missing
+# so we need to set it
+if [[ -v VIA_MONIT_USER ]]; then
+	 export $USER=$VIA_MONIT_USER
+fi
+	 
 $GS_HOME/bin/startNetldi via
