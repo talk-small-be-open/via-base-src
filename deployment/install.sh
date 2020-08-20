8f565c4710e60da3cdb5e70c44773ea4aab9b3e0
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2020-08-20 14:23:55 +0200

    Deployment

--- deployment/install.sh
+++ deployment/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-ansible-playbook -i ./site/inventory_$1.yml -e customerCode=$1 --ask-become-pass playbook_install.yml
+# Installs a via server from scratch
+#
+# Usage: ./install.sh <name of instance>
+# Example: ./install.sh production
+
+ansible-playbook -i ./site/inventory_$1.yml -e instanceName=$1 --ask-become-pass playbook_install.yml
