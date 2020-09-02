bc141857bdc6e9d40472f953098542462821494f
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2020-09-02 09:33:42 +0200

    progress

--- web_root/admin/js/scripts.js
+++ web_root/admin/js/scripts.js
@@ -3,6 +3,26 @@
 //
 
 
+function debounce(func, threshold, execAsap) {
+  var timeout;
+
+  return function debounced () {
+    var obj = this, args = arguments;
+    function delayed () {
+      if (!execAsap)
+        func.apply(obj, args);
+      timeout = null;
+    };
+
+    if (timeout)
+      clearTimeout(timeout);
+    else if (execAsap)
+      func.apply(obj, args);
+
+    timeout = setTimeout(delayed, threshold || 100);
+  };
+}
+
 /* Haupt JS init */
 $(document).ready(function(){
 
