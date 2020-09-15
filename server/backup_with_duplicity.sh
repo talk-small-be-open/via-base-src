99b1e61770893688ab46da6890b5baad53386ece
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2020-09-15 09:13:48 +0200

    progress

--- server/backup_with_duplicity.sh
+++ server/backup_with_duplicity.sh
@@ -6,7 +6,7 @@ STONE_NAME=$1
 
 
 # Get credentials for duplicity, if any
-source "/root/.duplicity.cred"
+source "/opt/via/.duplicity.cred"
 
 #
 # Run duplicity
@@ -18,15 +18,15 @@ BACKEND=$2
 
 # GemStone DB
 TARGET=$BACKEND/via_db
-duplicity --log-file /var/log/duplicity.log --verbosity info --archive-dir=/var/tmp/duplicity --no-print-statistics --full-if-older-than 7D --no-encryption --include "**/via_backup.dbf.gz" --exclude "**/*" $GS_HOME/server/stones/$STONE_NAME/backups $TARGET
-duplicity remove-all-but-n-full 3 --archive-dir=/var/tmp/duplicity --log-file /var/log/duplicity.log --force --verbosity info --no-print-statistics $TARGET
+duplicity --log-file /opt/via/log/duplicity.log --verbosity info --archive-dir=/opt/via/tmp/duplicity --no-print-statistics --full-if-older-than 7D --no-encryption --include "**/via_backup.dbf.gz" --exclude "**/*" $GS_HOME/server/stones/$STONE_NAME/backups $TARGET
+duplicity remove-all-but-n-full 3 --archive-dir=/opt/via/tmp/duplicity --log-file /opt/via/log/duplicity.log --force --verbosity info --no-print-statistics $TARGET
 
 # Assets
 TARGET=$BACKEND/via_assets
-duplicity --log-file /var/log/duplicity.log --verbosity info --archive-dir=/var/tmp/duplicity --no-print-statistics --full-if-older-than 7D --no-encryption /opt/via/via_base/web_root/assets $TARGET
-duplicity remove-all-but-n-full 3 --archive-dir=/var/tmp/duplicity --log-file /var/log/duplicity.log --force --verbosity info --no-print-statistics $TARGET
+duplicity --log-file /opt/via/log/duplicity.log --verbosity info --archive-dir=/opt/via/tmp/duplicity --no-print-statistics --full-if-older-than 7D --no-encryption /opt/via/via_base/web_root/assets $TARGET
+duplicity remove-all-but-n-full 3 --archive-dir=/opt/via/tmp/duplicity --log-file /opt/via/log/duplicity.log --force --verbosity info --no-print-statistics $TARGET
 
 # Exported module PDFs and others
 TARGET=$BACKEND/via_exports
-duplicity --log-file /var/log/duplicity.log --verbosity info --archive-dir=/var/tmp/duplicity --no-print-statistics --full-if-older-than 7D --no-encryption /opt/via/export $TARGET
-duplicity remove-all-but-n-full 3 --archive-dir=/var/tmp/duplicity --log-file /var/log/duplicity.log --force --verbosity info --no-print-statistics $TARGET
+duplicity --log-file /opt/via/log/duplicity.log --verbosity info --archive-dir=/opt/via/tmp/duplicity --no-print-statistics --full-if-older-than 7D --no-encryption /opt/via/export $TARGET
+duplicity remove-all-but-n-full 3 --archive-dir=/opt/via/tmp/duplicity --log-file /opt/via/log/duplicity.log --force --verbosity info --no-print-statistics $TARGET
