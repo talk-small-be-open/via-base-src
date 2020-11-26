73c6a2892e9401aa5fb5a39c574d60b5a3c10be8
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2020-11-26 10:02:41 +0100

    git hook template

---
+++ .githooks/fix-pharo-iceberg-filemodes.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This is a script to fix filesystem problems which are introduced
+# by Pharo/Iceberg. It seems that the used libgit2 is not handling filemodes
+# correctly. Even the git config filemode=false does not prevent Iceberg/libgit2
+# from resetting the filemodes of shell script files. Same for symlink files, which
+# are frequently reset to simple text files.
+#
+# Id did not find a solution, so this post-merge hook should be installed in the
+# git repo for Iceberg as symlinks from the .git/hook/post-merge file.
+
+# Keep the shell scripts executable
+chmod +x ./server/*.sh
+
+# .. or maybe better: git reset --hard server/
+
+# TDB ensure that symlinks are symlinks
